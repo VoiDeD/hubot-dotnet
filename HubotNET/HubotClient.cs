552077390dcfa38002bbda60390187e74192e188
Ryan Stecker <ryan@stecker.email>
2015-01-18 17:56:37 -0600

    Initial implementation of the client library.

---
+++ HubotNET/HubotClient.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubotNET
+{
+    public sealed class HubotClient
+    {
+        TcpClient client;
+
+        NetworkStream netStream;
+
+        BinaryReader binReader;
+        BinaryWriter binWriter;
+
+
+        public async Task Connect( string host, int port )
+        {
+            // TcpClient instances can't be re-used, so we create a new one for every connect attempt
+            client = new TcpClient();
+
+            await client.ConnectAsync( host, port );
+
+            netStream = client.GetStream();
+
+            binReader = new BinaryReader( netStream );
+            binWriter = new BinaryWriter( netStream );
+
+            // begin read loop
+            // variable assignment avoids CS4014 which we don't particularly care about right here
+            var fireAndForget = Read();
+        }
+
+        public Task SendChat( string sender, string message )
+        {
+            return SendPayload( PacketType.Chat, sender, message );
+        }
+
+        Task SendPayload( PacketType type, params string[] data )
+        {
+            using ( var ms = new MemoryStream() )
+            using ( var bw = new BinaryWriter( ms ) )
+            {
+                // serialize our payload into memory
+                // we're only working with (usually) small buffers so this isn't a huge hit on the GC to always do this
+                // and we have the benefit of knowing our buffer size without any calculations
+
+                bw.Write( (byte)type );
+
+                foreach ( string param in data )
+                {
+                    // we use our own encoding of strings
+                    bw.WriteSafeString( param );
+                }
+
+                byte[] payload = ms.ToArray();
+
+                // now send the payload over the wire
+
+                // todo: currently this is done serially to avoid any races
+                // this should eventually be changed to an async write
+                // but for now we're relying on the tcp stack to not block on a full send buffer
+
+                binWriter.Write( payload.Length );
+                binWriter.Write( payload );
+
+                return Task.FromResult( true );
+            }
+        }
+
+
+        async Task Read()
+        {
+            while ( true )
+            {
+                // read packet length off the stream
+                int packetLen = await binReader.ReadInt32Async();
+                byte[] payload = await binReader.ReadBytesAsync( packetLen );
+
+                // todo: process the payload
+            }
+        }
+    }
+
+    enum PacketType
+    {
+        Invalid = 0,
+
+        Chat,
+        Emote,
+        Topic
+    }
+}
---
+++ HubotNET/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubotNET
+{
+    static class BinaryExtensions
+    {
+        public static async Task<int> ReadInt32Async( this BinaryReader reader )
+        {
+            var buff = new byte[ 4 ];
+            await reader.BaseStream.ReadAsync( buff, 0, buff.Length );
+
+            return BitConverter.ToInt32( buff, 0 );
+        }
+
+        public static async Task<byte[]> ReadBytesAsync( this BinaryReader reader, int numBytes )
+        {
+            // NB! this is really only friendly for small buffers
+
+            var buff = new byte[ numBytes ];
+            await reader.BaseStream.ReadAsync( buff, 0, buff.Length );
+
+            return buff;
+        }
+
+
+        public static Task WriteAsync( this BinaryWriter writer, int value )
+        {
+            byte[] buff = BitConverter.GetBytes( value );
+            return writer.WriteAsync( buff );
+        }
+
+        public static Task WriteAsync( this BinaryWriter writer, byte[] buffer )
+        {
+            return writer.BaseStream.WriteAsync( buffer, 0, buffer.Length );
+        }
+
+
+        public static void WriteSafeString( this BinaryWriter writer, string value )
+        {
+            // our own utf8 string-to-stream serialization:
+            // int32 len
+            // byte[] data of len length
+
+            byte[] bytes = Encoding.UTF8.GetBytes( value );
+
+            writer.Write( bytes.Length );
+            writer.Write( bytes );
+        }
+    }
+}
